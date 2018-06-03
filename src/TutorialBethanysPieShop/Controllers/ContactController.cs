e9a6e232501984a7863b0dfb8182be63df07e4a2
Leon <yanglyu.leon.7@gmail.com>
2018-06-03 11:52:20 -0700

    demo: Creating a Custom TagHelper for the Contact Page
    module 6.8

---
+++ src/TutorialBethanysPieShop/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TutorialBethanysPieShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
